896d76cfd9402b132c4b4ba8da9a9760724babd1
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-09-01 20:27:06 +0430

    project templete added

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    localhost: { url: "http://127.0.0.1:8545" },
+    hardhat: {},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: { timeout: 40000 },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,10 @@
+const hre = require("hardhat");
+
+async function main() {
+  //deploy logic goes here
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
